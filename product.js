be724282d09e24dcd3dbefc4a68bc3ba69537a1e
kevinkoech38 <97313681+kevinkoech38@users.noreply.github.com>
2023-02-10 10:19:11 +0400

    first commit

---
+++ product.js
@@ -0,0 +1,52 @@
+let products =
+[
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," +
+            " organic goodness for your cat.",
+        price: 20,
+        image: "images/product-fullsize.jpg",
+        availableItemInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," +
+            " organic goodness for your cat.",
+        price: 10,
+        image: "images/product-fullsize.jpg",
+        availableItemInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1003,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," +" organic goodness for your cat.",
+        price: 15,
+        image: "images/product-fullsize.jpg",
+        availableItemInventory: 10,
+        rating: 1
+    },
+    {
+        id: 1004,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," +
+            " organic goodness for your cat.",
+        price: 16,
+        image: "images/product-fullsize.jpg",
+        availableItemInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1005,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," +
+            " organic goodness for your cat.",
+        price: 50,
+        image: "images/product-fullsize.jpg",
+        availableItemInventory: 10,
+        rating: 5
+    }
+]
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'petstore.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png' 
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');  
+    e.waitUntil(    
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);   
+         })  
+         );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log('[Service Worker] Fetched resource '+e.request.url);
+});
+
+self.addEventListener('fetch', function (e) {    
+    e.respondWith(
+        // check if the cache has the file        
+    caches.match(e.request).then(function (r) {console.log('[Service Worker] Fetching resource: '              
+    + e.request.url);
+    // 'r' is the matching file if it exists in the cachereturn r       
+    return r
+        })    
+    );
+});
+
